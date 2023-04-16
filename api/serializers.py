c73f558308cd2bec0aca24ebac6f1be9da12f37e
Hector Duran <dany0997@gmail.com>
2023-04-16 15:43:21 -0600

    formatting

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from images.models import Image
+
 # create serializer for Image that validates:
 # - file format allowed
 # - validate size no larger than 5MB
